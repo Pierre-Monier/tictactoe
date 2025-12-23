3fa4dd35b908439c745902b73f9d8804665a860a
Pierre Monier <pierre.monier-extern@renault.com>
2025-12-23 19:04:08 +0100

    feat: update colors

--- lib/presentation/ui/tictactoe_color_scheme.dart
+++ lib/presentation/ui/tictactoe_color_scheme.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:tictactoe/model/game_result.dart';
 
 extension TictactoeColor on ColorScheme {
-  Color get cross => primary;
-  Color get nought => secondary;
+  Color get cross => error;
+  Color get nought => primary;
   Color get draw => tertiary;
 
-  Color get onCross => onPrimary;
-  Color get onNought => onSecondary;
+  Color get onCross => onError;
+  Color get onNought => onPrimary;
   Color get onDraw => onTertiary;
 
   Color? fromGameResult(GameResult result) => switch (result) {
